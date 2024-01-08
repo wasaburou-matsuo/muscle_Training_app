b115badd12e896476a9b0c4bb00027403f02c4de
wasaburou-matsuo <kurawasa.fire@gmail.com>
2024-01-08 12:00:09 +0900

    ヘッダー・フッタ部分の共通化を行いました。

--- resources/views/layouts/announce-header.blade.php
+++ resources/views/layouts/announce-header.blade.php
@@ -6,8 +6,11 @@
     {{-- ディレクティブを使用して、ログイン状態で処理を分岐する --}}
     @auth
         {{-- {route('profile.edit')}は、laravelのルート関数で、URLに変換してくれる --}}
+        {{-- route関数を使わなければ、以下の記述となる。http://localhost:8080は、Laravelで自動補完してくれる。 --}}
+        {{-- <a href="/profile" class="ml-auto">マイページ</a> --}}
         <a href="{{route('profile.edit')}}" class="ml-auto">マイページ</a>
     @endauth
+    {{-- ログインしていない場合 --}}
     @guest
         <a href="{{route('register')}}" class="mr-2">ユーザー登録（無料）</a>
         <a href="{{route('login')}}" class="">ログイン</a>
