53dba5bf8edfe0ee35640ae2d88648920f0f9643
wasaburou-matsuo <kurawasa.fire@gmail.com>
2024-01-22 06:41:46 +0900

    画像選択ダイアログ表示を実装

--- resources/views/training_results/create.blade.php
+++ resources/views/training_results/create.blade.php
@@ -6,6 +6,7 @@
             <div class="grid grid-cols-2 rounded border border-gray-500 mt-4">
               <div class="col-span-1">
                 <img class="object-cover w-full aspect-video" src="/images/recipe-dummy.png" alt="recipe-image">
+                <input type="file" name="image" class="border border-gray-300 p-2 mb-4 w-full rounded">
               </div>
               <div class="col-span-1 p-4">
                 <input type="text" name="title" placeholder="トレーニング名" class="border border-gray-300 p-2 mb-4 w-full rounded">
@@ -13,7 +14,7 @@
                 <select name="category" class="border border-gray-300 p-2 mb-4 w-full rounded">
                   <option value="">カテゴリー</option>
                 @foreach ($areas as $a)
-                  <option value="{{ $a['id']}}">{{ $a['name']}}</opotin>
+                  <option value="{{ $a['id']}}">{{ $a['name']}}</option>
                 @endforeach
                   </select>
                 <div class="flex justify-end">
